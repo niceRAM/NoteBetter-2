09f699979e1d3b316e6c2d1c1828bcc749309577
niceRAM <niceniceram@gmail.com>
2019-03-13 16:15:07 +0800

    update to 1.12.2

--- src/main/scala/com/github/soniex2/notebetter/NoteBetter.java
+++ src/main/scala/com/github/soniex2/notebetter/NoteBetter.java
@@ -26,7 +26,7 @@ import java.io.*;
 @ParametersAreNonnullByDefault
 public class NoteBetter implements NoteBetterAPIInstance {
     public static final String MODID = "notebetter";
-    public static final String VERSION = "2.0.1";
+    public static final String VERSION = "2.1.0";
 
     @Mod.Instance
     public static NoteBetter instance;
